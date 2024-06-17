56bc6f4a46faf32ef8a84c1ac4b520d24f2274fe
Juan LB <ljuan7397@gmail.com>
2024-06-17 19:35:58 +0200

    chore: Update app.module.ts to include ConfigModule for environment variable setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { UserModule } from '@modules/user/user.module';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 
 @Module({
-	imports: [UserModule],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: `.env.${process.env.NODE_ENV}`,
+			isGlobal: true,
+		}),
+		UserModule,
+	],
 	controllers: [],
 	providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	const configService = app.get(ConfigService);
 
 	app.setGlobalPrefix('api');
 
@@ -16,6 +19,6 @@ async function bootstrap() {
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup('api-doc', app, document);
 
-	await app.listen(process.env.NEST_PORT || 3000);
+	await app.listen(configService.get('NEST_PORT'));
 }
 bootstrap();
