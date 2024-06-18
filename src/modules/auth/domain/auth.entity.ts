a9d75b468a5d86276ac13378db15f69fec30f37d
Juan LB <ljuan7397@gmail.com>
2024-06-18 09:33:30 +0200

    Add Auth entity

--- src/modules/auth/domain/auth.entity.ts
+++ src/modules/auth/domain/auth.entity.ts
@@ -1,7 +1,32 @@
+import { Entity, Result } from '@lib';
 import type { JwtToken } from './jwt-token.value-object';
 
 export interface AuthProps {
 	userId: string;
 	token: JwtToken;
 	refreshToken: JwtToken;
+}
+
+export class Auth extends Entity<AuthProps> {
+	get userId() {
+		return this.props.userId;
+	}
+
+	get token() {
+		return this.props.token;
+	}
+
+	get refreshToken() {
+		return this.props.refreshToken;
+	}
+
+	private constructor(props: AuthProps) {
+		super(props);
+	}
+
+	public static create(props: AuthProps): Result<Auth> {
+		const auth = new Auth(props);
+
+		return Result.ok(auth);
+	}
 }
