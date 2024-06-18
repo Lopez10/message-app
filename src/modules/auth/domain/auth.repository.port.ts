cbe233c5e50fd4591d25fffcdb2132b8e2049fc0
Juan LB <ljuan7397@gmail.com>
2024-06-18 09:39:34 +0200

    chore: Update AuthRepositoryPort with updateAuth method

--- src/modules/auth/domain/auth.repository.port.ts
+++ src/modules/auth/domain/auth.repository.port.ts
@@ -1,7 +1,8 @@
-import { Id } from '@lib';
-import { Auth } from './auth.entity';
+import type { Id } from '@lib';
+import type { Auth } from './auth.entity';
 
 export interface AuthRepositoryPort {
 	save(auth: Auth): Promise<void>;
 	findByUserId(userId: Id): Promise<Auth | null>;
+	updateAuth(auth: Auth): Promise<void>;
 }
