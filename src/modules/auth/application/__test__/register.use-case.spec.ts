b44d532eba23045849aa590987f83011fd751a5b
Juan LB <ljuan7397@gmail.com>
2024-06-19 10:37:39 +0200

    refactor: Add unit test for Register Use Case

---
+++ src/modules/auth/application/__test__/register.use-case.spec.ts
@@ -0,0 +1,8 @@
+describe('Register Use Case', () => {
+	it(`
+        GIVEN a valid email, password and name
+        WHEN I call the register use case
+        THEN I should receive a token
+        AND the user should be created in the database
+    `, async () => {});
+});
