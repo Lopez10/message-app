1308124956e3015e9b83e59cd80c760f4eab2ded
Juan LB <ljuan7397@gmail.com>
2024-06-19 19:55:08 +0200

    refactor: Update GetActiveUsers use case to fetch active users from the database

--- src/modules/user/presentation/user.controller.ts
+++ src/modules/user/presentation/user.controller.ts
@@ -1,14 +1,15 @@
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
-import { UserMongoRepository } from '../infrastructure/user.postgre.repository';
+import { UserMongoRepository as UserPostgreRepository } from '../infrastructure/user.postgre.repository';
 import { UserRepositoryPortSymbol } from '../domain/user.repository.port';
 import { Controller, Get, Inject } from '@nestjs/common';
+import { GetActiveUsers } from '../application/get-active-users/get-active-users.use-case';
 
 @ApiTags('user')
 @Controller('user')
 export class UserController {
 	constructor(
 		@Inject(UserRepositoryPortSymbol)
-		private readonly userRepository: UserMongoRepository,
+		private readonly userRepository: UserPostgreRepository,
 	) {}
 
 	@Get('active')
@@ -16,5 +17,16 @@ export class UserController {
 		status: 200,
 		description: 'List of active users',
 	})
-	async active() {}
+	async active() {
+		const getActiveUsers = new GetActiveUsers(this.userRepository);
+
+		const result = await getActiveUsers.run();
+
+		if (result.isLeft()) {
+			const error = result.getLeft();
+			throw error;
+		}
+
+		return result.get();
+	}
 }
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '@modules/prisma/prisma.module';
 import { UserMongoRepository } from './infrastructure/user.postgre.repository';
 import { UserRepositoryPortSymbol } from './domain/user.repository.port';
+import { UserController } from './presentation/user.controller';
 @Module({
 	imports: [PrismaModule],
-	controllers: [],
+	controllers: [UserController],
 	providers: [
 		{
 			provide: UserRepositoryPortSymbol,
