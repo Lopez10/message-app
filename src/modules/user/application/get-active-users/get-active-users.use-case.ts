acb21ffa20ca4efe6d46b8e829cd8a2de031f57a
Juan LB <ljuan7397@gmail.com>
2024-06-19 19:51:01 +0200

    refactor: Update GetActiveUsers use case to fetch active users from the database

--- src/modules/user/application/get-active-users/get-active-users.use-case.ts
+++ src/modules/user/application/get-active-users/get-active-users.use-case.ts
@@ -1,12 +1,22 @@
 import { Either, UseCase } from '@lib';
-import { UserPrimitives } from '@modules/user/domain/user.entity';
-import { Injectable } from '@nestjs/common';
+import { User, UserPrimitives } from '@modules/user/domain/user.entity';
+import { UserRepositoryPortSymbol } from '@modules/user/domain/user.repository.port';
+import { UserMongoRepository } from '@modules/user/infrastructure/user.postgre.repository';
+import { Inject, Injectable } from '@nestjs/common';
+import { UserMapper } from '../user.mapper';
 
 @Injectable()
 export class GetActiveUsers
 	implements UseCase<void, Either<void, UserPrimitives[]>>
 {
-	run(): Promise<Either<void, UserPrimitives[]>> {
-		throw new Error('Method not implemented.');
+	constructor(
+		@Inject(UserRepositoryPortSymbol)
+		private readonly userRepository: UserMongoRepository,
+	) {}
+
+	async run(): Promise<Either<void, UserPrimitives[]>> {
+		const users: User[] = await this.userRepository.getActiveUsers();
+
+		return Either.right(users.map(UserMapper.toDto));
 	}
 }
