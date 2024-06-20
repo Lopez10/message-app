fb8f0fe56d5975cb37d9a16814cbf4cb94a71efe
Juan LB <ljuan7397@gmail.com>
2024-06-20 10:11:30 +0200

    chore: Add UserModule and AuthModule to MessageModule imports

--- src/modules/message/message.module.ts
+++ src/modules/message/message.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '@modules/prisma/prisma.module';
+import { UserModule } from '@modules/user/user.module';
+import { AuthModule } from '@modules/auth/auth.module';
 @Module({
-	imports: [PrismaModule],
+	imports: [PrismaModule, UserModule, AuthModule],
 	controllers: [],
 	providers: [],
 })
